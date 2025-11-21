c707fbba24d83a6995fad6cfcda45724496e465b
unssely <susarinaulia91@gmail.com>
2025-11-21 13:02:21 +0500

    card

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import './style.css'
+import 'flowbite';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
+    let swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      centeredSlides: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
